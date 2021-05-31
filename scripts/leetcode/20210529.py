e9a8b6b06223f32e12635cff7071cf5b2b2a9f7d
songpeiyao <songpeiyao@changingedu.com>
2021-05-31 10:07:06 +0800

    cc

---
+++ scripts/leetcode/20210529.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# @Project : DataMachine_for_timers
+# @File    : 20210529.py
+# @Desc    : 
+# @Time    : 2021/5/29 15:03
+# @Author  : songpeiyao
+# @Version : 1.0
+from typing import List
+
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+
+        return
+
+
+
+
+
+
+
---
+++ scripts/used_mulprocessing.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# @Project : DataMachine_for_timers
+# @File    : used_mulprocessing.py
+# @Desc    : 
+# @Time    : 2021/5/29 15:23
+# @Author  : songpeiyao
+# @Version : 1.0
+
+
