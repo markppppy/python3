5a9443449c5cfa015291d9570d6673d463249cda
songpeiyao <songpeiyao@changingedu.com>
2021-06-02 18:42:08 +0800

    cc

--- scripts/leetcode/20210602.py
+++ scripts/leetcode/20210602.py
@@ -14,6 +14,58 @@ from typing import List
 
 
 def minSubArrayLen(target: int, nums: List[int]) -> int:
-    print()
-    return 1
+    # 定义两个指针i j;
+    # i不动，j增加，当i~j的和大于等于target时，j不在变，记录长度；
+    # i+1，。。。
+    # 循环
+    i = 0
+    j = 0
+    minlen = len(nums)+1
+    while i < len(nums):
+        while j < len(nums):
+            if sum(nums[i: j+1]) >= target:
+                minlen = min(minlen, j-i+1)
+                break
+            j += 1
+        i += 1
+    if minlen == len(nums)+1:
+        minlen = 0
+    return minlen
 
+
+def minSubArrayLen2(s, nums):
+    """
+    相比 minSubArrayLen 减少不必要的遍历
+    :type s: int
+    :type nums: List[int]
+    :rtype: int
+    """
+
+    n = len(nums)
+    if n < 1 or sum(nums) < s:
+        return 0
+
+    # 维护一个滑动窗口nums[i,j], nums[i...j] < s
+    i = 0
+    j = -1
+    total = 0
+    res = n + 1
+    while i <= n-1:
+        if (j + 1 < n) and total < s:
+            j += 1
+            total += nums[j]
+        else:
+            total -= nums[i]
+            i += 1
+
+        if total >= s:
+            res = min(res, j-i+1)
+    if res == n+1:
+        return 0
+    return res
+
+
+if __name__ == '__main__':
+    target = 7
+    nums = [2, 3, 1, 2, 4, 3]
+    print(minSubArrayLen2(target, nums))
---
+++ scripts/leetcode/sort_methods.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# @Project : DataMachine_for_timers
+# @File    : sort_methods.py
+# @Desc    : 
+# @Time    : 2021/6/2 17:20
+# @Author  : songpeiyao
+# @Version : 1.0
+
+
+
