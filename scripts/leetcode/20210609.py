0ba443c1d8a8c2ea5f0b823002faa9dbd6207aca
songpeiyao <songpeiyao@changingedu.com>
2021-06-08 19:45:20 +0800

    cc

---
+++ scripts/leetcode/20210609.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# @Project : DataMachine_for_timers
+# @File    : 20210609.py
+# @Desc    : 
+# @Time    : 2021/6/8 19:36
+# @Author  : songpeiyao
+# @Version : 1.0
+"""
+最长公共前缀
+
+编写一个函数来查找字符串数组中的最长公共前缀。
+如果不存在公共前缀，返回空字符串 ""。
+
+输入：strs = ["flower","flow","flight"]
+输出："fl"
+"""
+
+
