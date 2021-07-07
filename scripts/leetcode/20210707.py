9cbc34512de28fbdebd2847b8a8d34043e937852
songpeiyao <songpeiyao@changingedu.com>
2021-07-07 19:57:17 +0800

    cc

---
+++ scripts/leetcode/20210707.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# @Project : DataMachine_for_timers
+# @File    : 20210707.py
+# @Desc    : 
+# @Time    : 2021/7/7 19:13
+# @Author  : songpeiyao
+# @Version : 1.0
