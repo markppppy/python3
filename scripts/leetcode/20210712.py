eb814b9f9b4b7e1792ad2d57953aa5fbedf1c21c
songpeiyao <songpeiyao@changingedu.com>
2021-07-13 20:40:21 +0800

    1

--- scripts/leetcode/20210712.py
+++ scripts/leetcode/20210712.py
@@ -23,42 +23,88 @@ deleteAtIndex(index)：如果索引index 有效，则删除链表中的第index 
 """
 
 
+class Node_:  # 不继承 object 可以吗?
+    def __init__(self, x):
+        self.val = x
+        self.next = None  # .next 是一个对象
+
+
 class MyLinkedList_:
 
     def __init__(self):
         """
         Initialize your data structure here.
         """
+        self.head = Node(0)  # head.val=0, head.next=None
         # self = [{}] ?
 
-    def get(self, index: int) -> int:
+    def get(self, index: int) -> int:  # index 等于 0 时, 是什么含义?
         """
         Get the value of the index-th node in the linked list. If the index is invalid, return -1.
         """
-        # 怎么使用主体?
-        # if index <= len(self):
-        # return self[index]
+        node = self.head
+        if index < 0:  # index 是从0开始的
+            return -1
+        for _ in range(index+1):
+            if node is not None:
+                node = node.next
+            else:
+                return -1
+        return node.val
 
     def addAtHead(self, val: int) -> None:
         """
         Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
         """
+        _a = Node(val)  # 新增的值怎么定义? 就这样任意初始化一个变量
+        _a.next = self.head.next
+        self.head.next = _a
 
     def addAtTail(self, val: int) -> None:
         """
         Append a node of value val to the last element of the linked list.
         """
+        # 从 self.head 一直取到最后
+        node = self.head
+        while node.next is not None:
+            node = node.next  # 怎么取到另一个值?  node.next是一个对象
+
+        node.next = Node(val)
 
     def addAtIndex(self, index: int, val: int) -> None:
         """
         Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list.
         If index is greater than the length, the node will not be inserted.
         """
+        node = self.head
+        for _ in range(index):
+            if node is not None:
+                node = node.next
+            else:
+                return  # If index is greater than the length, the node will not be inserted.
+        if node is None:  # 证明index-1位置是尾节点
+            node = Node(val)
+        else:
+            new = Node(val)
+            new.next = node.next
+            node.next = new
 
     def deleteAtIndex(self, index: int) -> None:
         """
         Delete the index-th node in the linked list, if the index is valid.
         """
+        if index < 0:
+            return
+        node = self.head
+        for _ in range(index):
+            if node is not None:
+                node = node.next
+            else:
+                return
+        if node is not None:
+            ndel = node.next
+            node.next = ndel.next
+            ndel.next = None
 
 
 class Node(object):
@@ -67,8 +113,8 @@ class Node(object):
         self.next = None
 
 
-a = Node(2)
-print(a.val)
+# a = Node(2)
+# print(a.val)
 
 
 class MyLinkedList:
@@ -77,7 +123,8 @@ class MyLinkedList:
         """
         Initialize your data structure here.
         """
-        self.head = Node(0)
+        # 类实例化的时候，会给对象一个属性
+        self.head = Node(0)  # Node(0) 表示Node类的实例化
 
     def get(self, index: int) -> int:
         """
@@ -136,6 +183,10 @@ class MyLinkedList:
         if node.next is not None:
             node.next = node.next.next
 
+
+if __name__ == '__main__':
+    a = MyLinkedList()
+    print(a.head)
 # Your MyLinkedList object will be instantiated and called as such:
 # obj = MyLinkedList()
 # param_1 = obj.get(index)
