d2088ca39c9f5be8db1b8d1d7614ad97e8f014a0
songpeiyao <songpeiyao@changingedu.com>
2021-08-21 12:08:37 +0800

    cc

--- scripts/leetcode/20210812.py
+++ scripts/leetcode/20210812.py
@@ -18,19 +18,22 @@ s 仅由小写英文字母组成
 
 class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
-        # 标记每个字符，如果一个字符出现两次，则有一个长度为3的回文子串
-        # 如果有两个字符出现两次，且顺序相反或字符相等，则有 2*2+1个回文子串
-        # 对撞指针
         n = len(s)
-        i = 0
-        max_sub = 0
-        for i in range(0, n):
-            for j in range(1, n):
-                if i < j:
-                    if s[i] == s[-j]:
-                        max_sub += 1
-                        i += 1
-                        j += 1
+        dp = [[0] * n for _ in range(n)]
+        for i in range(n - 1, -1, -1):
+            dp[i][i] = 1
+            for j in range(i + 1, n):
+                if s[i] == s[j]:
+                    dp[i][j] = dp[i + 1][j - 1] + 2
+                else:
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j - 1])
+        return dp[0][n - 1]
 
+
+if __name__ == '__main__':
+    s = "bbbab"
+    s_example = Solution()
+    s_example.longestPalindromeSubseq(s)
+    print()
 
 
