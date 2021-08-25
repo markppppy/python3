3143be82291fac02916e3a841595a532e1712af9
songpeiyao <songpeiyao@changingedu.com>
2021-08-25 19:25:10 +0800

    11

--- scripts/funnel_graph.py
+++ scripts/funnel_graph.py
@@ -8,8 +8,9 @@
 # @Version : 1.0
 
 import pandas as pd
-from pyecharts import Funnel
+from pyecharts.charts import Funnel
 import numpy as np
+from pyecharts import options as opts
 
 df = pd.DataFrame(data=[{'stage': '计划送检课程预计上课时长', 'course_hour': 171320.25, 'rate': 1}
                         , {'stage': '出席课程的预计上课时长', 'course_hour': 147472.75, 'rate': 0.86}
@@ -20,14 +21,18 @@ df = pd.DataFrame(data=[{'stage': '计划送检课程预计上课时长', 'cours
 attrs = df['stage'].tolist()
 attr_value = (np.array(df['rate']) * 100).tolist()
 
-funnel = Funnel("检测时长损失分析", width=800, height=400, title_pos='center')
-funnel.add(name="课程送检环节",  # 指定图例名称
-           attr=attrs,  # 指定属性名称
-           value=attr_value,  # 指定属性所对应的值
-           is_label_show=True,  # 指定标签是否显示
-           label_formatter='{b}{c}%',  # 指定标签显示的格式
-           label_pos="outside",  # 指定标签的位置
-           is_legend_show=False)  # 指定图例不显示图例
-# todo 还没没有画出满意的漏斗图
-funnel.render()
+# pyecharts==1.9.0
+# https://pyecharts.org/#/zh-cn/intro
+
+c = (
+    Funnel(init_opts=opts.InitOpts(width="1600px", height="800px"))
+    .add(
+        "课程送检环节"
+        , [list(z) for z in zip(attrs, attr_value)]
+        , sort_="descending"
+        , label_opts=opts.LabelOpts(position="inside", formatter='{b}:{c}%')
+    )
+    .set_global_opts(title_opts=opts.TitleOpts(title="课程送检环节"))
+    .render("funnel_sort_desc.html")  # 会在同目录下生成文件
+)
 
