a4895e75549d11d698f0da6e5348dbd34bc710cb
markppppy <ppppy161@qq.com>
2021-10-22 17:51:01 +0800

    cc

--- scripts/test.py
+++ scripts/test.py
@@ -1,1 +1,12 @@
-max(1, 2, 3)
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# @Project : DataMachine_for_timers
+# @File    : lgbm_eval_obj_by_f1.py
+# @Desc    : 
+# @Time    : 2021/8/3 9:44
+# @Author  : songpeiyao
+# @Version : 1.0
+
+a = max(1, 2, 3)
+print(a) 
+
