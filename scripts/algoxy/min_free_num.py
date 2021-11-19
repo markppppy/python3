9ffcd552c4a5bb633d07b4441d8b2ab6c84c3694
markppppy <ppppy161@qq.com>
2021-11-19 18:39:26 +0800

    cc

---
+++ scripts/algoxy/min_free_num.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+# @Directory: D:\Document\local_rps\python3\scripts\algoxy
+# @File     : min_free_num.py
+# @Time     : 2021/11/17 10:23:39
+# @Author   : songpeiyao 
+# @Version  : 1.0
+# @Contact  : ppppy161@qq.com
+# @Desc     : 一串数字中找出没有出现的最小数字
+
+
+
